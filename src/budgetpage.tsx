863b76a86538e5f21c5a35c4d3213cd23395e946
thaarini2 <e1354860@u.nus.edu>
2025-05-21 23:58:18 +0800

    Create Goal

--- src/budgetpage.tsx
+++ src/budgetpage.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import SetBudget from './components/SetBudget';
+import SetGoal from './components/SetGoal';
 
 function BudgetPage() {
   return (
     <div>
-      <h2>Budget:</h2>
+      <h2>Budget set:</h2>
       <SetBudget />
 
       <h2>Goals and Challenges:</h2>
+      <SetGoal />
     </div>
   );
 }
